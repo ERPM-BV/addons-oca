e2777b5f7f522121568799981926471faea556b7
AlexanderMurre-Galileo <alexander.murre@galileo-itservices.nl>
2025-05-27 08:07:39 +0000

    wip1

--- account_move_line_attachment_preview/models/account_move_line.py
+++ account_move_line_attachment_preview/models/account_move_line.py
@@ -5,11 +5,13 @@ from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
-    _name = "account.move.line"
     _inherit = "account.move.line"
 
     preview_attachment_id = fields.Many2one(
         string="Main Attachment",
         comodel_name="ir.attachment",
         related="move_id.message_main_attachment_id",
+    )
+    attachment_ids = fields.One2many(
+        comodel_name="ir.attachment", related="move_id.attachment_ids"
     )
